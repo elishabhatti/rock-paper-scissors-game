77efe4c654e1eb61b067c509086e5d0be1886723
Elisha <elishakumari2007@gmail.com>
2024-11-26 12:16:41 +0500

    added reset button && styling

--- src/Components/RockPaperScissors.jsx
+++ src/Components/RockPaperScissors.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const RockPaperScissors = () => {
   const choices = [
@@ -6,11 +6,13 @@ const RockPaperScissors = () => {
     { name: "Paper", image: "/images/Paper.png" },
     { name: "Scissors", image: "/images/scissors.png" },
   ];
+  
   const [userSelection, setUserSelection] = useState("");
   const [computerSelection, setComputerSelection] = useState("");
   const [myScore, setMyScore] = useState(0);
   const [compScore, setCompScore] = useState(0);
   const [result, setResult] = useState("");
+  const [time, setTime] = useState(0)
 
   const playGame = (userSelect) => {
     const randomIndex = Math.floor(Math.random() * choices.length);
@@ -28,30 +30,48 @@ const RockPaperScissors = () => {
       (user === "Paper" && computer === "Rock") ||
       (user === "Scissors" && computer === "Paper")
     ) {
-      setResult("You Wins");
+      setResult("You Wins  🎉");
       setMyScore(myScore + 1);
     } else {
-      setResult("Computer Wins");
+      setResult("Computer Wins 💻");
       setCompScore(compScore + 1);
     }
+    // if (myScore >= 10 && compScore <= 10) {
+    //   alert("You Won");
+    //   handleRefresh();
+    // } else if (myScore <= 10 && compScore >= 10) {
+    //   alert("You Lose");
+    //   handleRefresh();
+    // }
   };
 
   const handleRefresh = () => {
     setMyScore(0);
     setCompScore(0);
+    setResult("");
+    setUserSelection("");
+    setComputerSelection("");
   };
 
   return (
     <div className="bg-gray-800 text-white h-[100vh] ">
-      <div className="w-full bg-slate-600 text-white flex justify-between items-center py-4 px-8 mb-20">
+      <div className="w-full bg-slate-600 text-white flex justify-between items-center py-4 px-8 mb-10">
         <div>
-          <h1 className="text-2xl uppercase">
-            Rock Paper Scissors Game:
-          </h1>
+          <h1 className="text-2xl uppercase">Rock Paper Scissors Game</h1>
         </div>
         <div>
-          <h2>User Score: <b> {myScore}</b></h2>
-          <h2>Computer Score: <b>{compScore}</b></h2>
+
+        </div>
+        <div>
+          <h2>
+            User Score: <b> {myScore}</b>
+          </h2>
+          <h2>
+            Computer Score: <b>{compScore}</b>
+          </h2>
+          <h2>
+            Timer: <b>{time}</b>
+          </h2>
         </div>
       </div>
       <div className="flex justify-center items-center flex-col ">
@@ -71,11 +91,24 @@ const RockPaperScissors = () => {
           ))}
         </div>
         <div>
-          <h2 className="text-[20px] uppercase">Your Choice: <b>{userSelection.name}</b></h2>
-          <h2 className="text-[20px] uppercase">Computer Choice: <b>{computerSelection.name}</b></h2>
-          <h2 className="text-[20px] uppercase">Result: <b>{result}</b></h2>
+          <h2 className="text-[20px] uppercase">
+            Your Choice: <b>{userSelection.name}</b>
+          </h2>
+          <h2 className="text-[20px] uppercase">
+            Computer Choice: <b>{computerSelection.name}</b>
+          </h2>
+          <h2 className="text-[20px] uppercase mt-10">
+            Result: <b>{result}</b>
+          </h2>
           <div className="flex justify-center items-center flex-col">
-          {result && <button className="fixed bottom-14  bg-white py-2 px-4 rounded-md text-black" onClick={handleRefresh}>Reset Score</button>}
+            {result && (
+              <button
+                className="fixed bottom-14  bg-white py-2 px-4 rounded-md text-black"
+                onClick={handleRefresh}
+              >
+                Reset Score
+              </button>
+            )}
           </div>
         </div>
       </div>
