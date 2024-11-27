557817b88a0539583175b3bdfd1b06e30de04d0f
Elisha <elishakumari2007@gmail.com>
2024-11-27 11:22:33 +0500

    fix bug of compputer score && name change

--- src/Components/RockPaperScissors.jsx
+++ src/Components/RockPaperScissors.jsx
@@ -14,7 +14,7 @@ const RockPaperScissors = () => {
   const [myScore, setMyScore] = useState(0);
   const [compScore, setCompScore] = useState(0);
   const [result, setResult] = useState("");
-  const [gameOver, setGameOver] = useState(false)
+  const [gameOver, setGameOver] = useState(false);
 
   const playGame = (userSelect) => {
     const randomIndex = Math.floor(Math.random() * choices.length);
@@ -31,22 +31,18 @@ const RockPaperScissors = () => {
       (user === "Paper" && computer === "Rock") ||
       (user === "Scissors" && computer === "Paper")
     ) {
-      setMyScore(myScore + 1);
-    }else if(user === computer) {
-      setMyScore(prevScore => prevScore )
-      setCompScore(prevScore => prevScore )
-    } 
-    else {
-      setCompScore(compScore + 1);
+      setMyScore((prevScore) => prevScore + 1);
+    } else if (user !== computer) {
+      setCompScore((prevScore) => prevScore + 1);
     }
-    if(myScore >= 10 && compScore <= 10) {
-      setResult("Congratulations You Won!")
-      setGameOver(true)
-    }else if(myScore <= 10 && compScore >= 10) {
-      setResult("You Loss!")
-      setGameOver(true)
-    } else if(myScore === 10 && compScore === 10) {
-      setResult("Its a Draw")
+    if (myScore >= 10 && compScore <= 10) {
+      setResult("Congratulations You Won!");
+      setGameOver(true);
+    } else if (myScore <= 10 && compScore >= 10) {
+      setResult("You Loss!");
+      setGameOver(true);
+    } else if (myScore === 10 && compScore === 10) {
+      setResult("Its a Draw");
     }
   };
 
@@ -56,14 +52,14 @@ const RockPaperScissors = () => {
     setResult("");
     setUserSelection("");
     setComputerSelection("");
-    setGameOver(false)
+    setGameOver(false);
   };
 
   return (
     <div className="bg-gray-800 text-white h-[100vh] ">
       <div className="w-full bg-slate-600 text-white flex justify-between items-center py-4 px-8 mb-10">
         <div>
-          <h1 className="text-[20px] uppercase">Rock Paper Scissors Game</h1>
+          <h1 className="text-[20px] uppercase">R.P.S Game</h1>
         </div>
         <div>
           <Score lable="Your Score" score={myScore} />
@@ -88,17 +84,27 @@ const RockPaperScissors = () => {
           ))}
         </div>
         <div>
-          <h2 className="text-[25px] mb-10 uppercase">The Wining Score is <b>10</b></h2>
+          <h2 className="text-[25px] mb-10 uppercase">
+            The Wining Score is <b>10</b>
+          </h2>
           <Choice label="Your Choice" choice={userSelection.name} />
           <Choice label="Computer Choice" choice={computerSelection.name} />
-            <h2 className={`text-[20px] mt-10 ${
-              result === "Congratulations You Won!" ? "text-green-500" : "text-red-500"
-            }`}>{result}</h2>
+          <h2
+            className={`text-[20px] mt-10 ${
+              result === "Congratulations You Won!"
+                ? "text-green-500"
+                : "text-red-500"
+            }`}
+          >
+            {" "}
+            <span className="text-white">Result: </span>
+            {result}
+          </h2>
           <div className="flex justify-center items-center flex-col">
             {result && (
               <button
-              className="fixed bottom-14 bg-white py-2 px-4 rounded-md text-black"
-              onClick={handleRefresh}
+                className="fixed bottom-14 bg-white py-2 px-4 rounded-md text-black"
+                onClick={handleRefresh}
               >
                 Play Again
               </button>
